76f428d9f71cdc4c12e5b4bee0c6714332f307cc
daniel <danielt@chain-reaction.io>
2023-08-25 01:08:12 +0300

    Ex3 solved

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+#copy original file to backup file
+cp /etc/apache2/sites-enabled/000-default.conf  /etc/apache2/sites-enabled/000-default.conf.bak
+
+#print conf content
+cat /etc/apache2/sites-enabled/000-default.conf
+
+#replace from Require all deny to Require all granted
+sudo sed -i 's/Require all deny/Require all granted/' /etc/apache2/sites-enabled/000-default.conf
+
+#restart apache2 service
+systemctl restart apache2
+
+
 #add fix to exercise3 here
+The problem was inside the conf file /etc/apache2/sites-enabled/000-default.conf
+
+The option "Require all" was configured for denied.
+As I changed to "Require all granted" I succeed to browse to the website.
