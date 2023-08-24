a4514db54b200477c024a89f05a6c3234b8fc018
daniel <danielt@chain-reaction.io>
2023-08-25 02:35:54 +0300

    add answer for ex5

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+
+#Create ssh certificate
+ssh-keygen -t rsa
+
+#Print id_rsa.pub content for copy to server2 
+cat .ssh/id_rsa.pub
+
+#Edit the SSH configuration file to disable strict host key checking.
+echo -e "Host server2\n\tStrictHostKeyChecking no" >> ~/.ssh/config
+
+
+
 #add fix to exercise5-server1 here
+I created a ssh certificate usgin the keygen.
+Then I print the content for id_rsa.pub for copy to the server2.
+There a command to copy directly from server1 to server2 but according to the ex we dont have the user password for the server2.
+Then I disable the strict host key check.
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+#Write id_rsa.pub content from server1 
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQChlu/30zJaWOYRf5qADamophC53shSrGWFaCcBXuNl0TE5hGBnYa/aAhFC1Kv/+9rboaOwicu8cf9ajcJAxggXZWLmj856SMuTKTWPaOkYSl0WYPIsJmnNrV0ATYtrNWnd+N2PU0Axa78TuKsSO3r+ugNMdS41pfwx2sSKFcpaHZaUi5OpOrBdv0zk3EgNPiC6PFvQbbdnIkuy81UtUT4y4DthTUPeBdVXFlMkv6oI/2cRND4wcGYhastj/G8lXyMOSTjEtRzUl+RfvAlUuvs43VLEjKFPO4uq5TKy+538HlhoIgoow9S8iIK9e4hUInuctv9l5cDGHuWYPtTSgluyMmfvIj2MSuXUs0WjQSSjNy9q0VnOmK0menkBbwMxzJ1N/o58kXykUIFjPzDIz4JDwblAVoF6SVZ2RKCxrS+KBALaU/24WZhUoDhIjcPR3orGkHXm7sMqf+7iT2+757FHTy6OtJtg3aSTRL1tPN5mumEm/leKt5N85ktgjt2W0is= vagrant@server1" >> ~/.ssh/authorized_keys
+
+#Disable StrictHostKeyChecking
+echo "StrictHostKeyChecking no" | sudo tee -a /etc/ssh/ssh_config
+
+#Restart ssh service
+sudo service ssh restart
+
 #add fix to exercise5-server2 here
+Add rsa_pub contect from server1 to authorized_keys file on server2
+
+Disable hostkey checking on server2
+
+restart ssh service for changes will take effect.
