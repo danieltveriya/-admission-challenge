8824516f86779009cde9f4225d540faa6ad50c77
daniel <danielt@chain-reaction.io>
2023-08-25 01:41:54 +0300

    Answer and finish ex4

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-#add fix to exercise4-server2 here
+#Add to /etc/hosts ip and record for server2
+echo "192.168.60.10 server1" | sudo tee -a /etc/hosts
+
+
+#add fix to exercise4-server1 here
+/etc/hosts is a text file used by operating systems including windows to map IP addresses to host names/domain names.
+I added server1 namd and ip, after this change i can connect to server1 via ssh with the name I configure at /etc/hosts
